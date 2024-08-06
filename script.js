ace71b611595ee23ed2d6385b4d53a237ed53a46
Easy×͜×Ice <116710976+3asyice@users.noreply.github.com>
2024-08-06 00:51:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificarCredenciaiss() {
   const user = document.getElementById('username').value;
   const pwd = document.getElementById('password').value;
 
-  if (user === "lixo" && pwd === "soares") {
+  if (user === "6° Brigada de Infantaria Blindada" && pwd === "Jack") {
     // Armazene a informação de login (não seguro)
     localStorage.setItem('loggedIn', 'true');
     window.location.href = "https://www.big-loser.site"; // Redirecionamento para o novo domínio
